5ea88ba3149858a2212dd6fccafc9cd9379b1593
sn-ekstrand <sn.ekstrand@gmail.com>
2020-04-12 13:04:53 -0500

    working on cleaning and organizing file

--- src/apartments_com_parser.py
+++ src/apartments_com_parser.py
@@ -20,32 +20,35 @@ request_header = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
     }
 
-# cities = ['austin-tx', 'new-york-ny', 'san-francisco-ca', 'boston-ma', 'seattle-wa', 'portland-or', 'phoenix-az', 'el-paso-tx', 'oklahoma-city-ok', 'indianapolis-in']
+def scrape_city(city):        
+    # Iterate through all pages for a city
+    # The max number of pages appears to always be 28
+    for page in range(1, 28+1):
+        base_url = "https://www.apartments.com/seattle-wa/{}/".format(page)
+        r = requests.get(base_url, headers=request_header)
+        time.sleep(np.random.randint(20, 45))
 
+        # save to mongodb
+        city_pages_table.insert_one({'city': 'seattle-wa', 'page_number': (page), 'url': base_url, 'html': r.text})
+
+        # grab just the html for the specific apartments on the page
+        soup = BeautifulSoup(r.text, 'html.parser')
+        placards = soup.find_all('article', 'placard')
+        for idx, apt in enumerate(placards):
+            tag = placards[idx]
+            sub_page_url = tag['data-url'] 
+            listing_id = tag['data-listingid']
+            sub_page_html = requests.get(sub_page_url, headers=request_header)
+            time.sleep(np.random.randint(25, 50))
+
+            # append sub page html to MongoDB
+            ind_apts_table.insert_one({'city': 'seattle-wa', 'listing_id': listing_id, 'url': sub_page_url, 'html': sub_page_html.text})
+
+        time.sleep(np.random.randint(10, 23))
+
+
+# cities = ['austin-tx', 'new-york-ny', 'san-francisco-ca', 'boston-ma', 'seattle-wa', 'portland-or', 'phoenix-az', 'el-paso-tx', 'oklahoma-city-ok', 'indianapolis-in']
 cities = ['austin-tx', 'san-francisco-ca', 'indianapolis-in']
 
-# for city in cities:
-# Iterate through all pages for a city
-# The max number of pages appears to always be 28
-for page in range(1, 28+1):
-    base_url = "https://www.apartments.com/seattle-wa/{}/".format(page)
-    r = requests.get(base_url, headers=request_header)
-    time.sleep(np.random.randint(20, 45))
-
-    # save to mongodb
-    city_pages_table.insert_one({'city': 'seattle-wa', 'page_number': (page), 'url': base_url, 'html': r.text})
-
-    # grab just the html for the specific apartments on the page
-    soup = BeautifulSoup(r.text, 'html.parser')
-    placards = soup.find_all('article', 'placard')
-    for idx, apt in enumerate(placards):
-        tag = placards[idx]
-        sub_page_url = tag['data-url'] 
-        listing_id = tag['data-listingid']
-        sub_page_html = requests.get(sub_page_url, headers=request_header)
-        time.sleep(np.random.randint(25, 50))
-
-        # append sub page html to MongoDB
-        ind_apts_table.insert_one({'city': 'seattle-wa', 'listing_id': listing_id, 'url': sub_page_url, 'html': sub_page_html.text})
-
-    time.sleep(np.random.randint(10, 23))
+for city in cities:
+    scrape_city(city)
